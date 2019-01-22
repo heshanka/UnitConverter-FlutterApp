9c1fdf0e966250985231be2e835a7ce1f5d8912b
Heshanka <heshanka123@gmail.com>
2019-01-22 19:20:37 +0530

    Made ConverterScreen stateful

--- category_route/lib/converter_screen.dart
+++ category_route/lib/converter_screen.dart
@@ -2,7 +2,7 @@ import 'package:category_route/Unit.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
-class ConverterScreen extends StatelessWidget{
+class ConverterScreen extends StatefulWidget{
 
   final String name;
   final List<Unit> units;
@@ -17,23 +17,26 @@ class ConverterScreen extends StatelessWidget{
         assert(name!=null);
 
   @override
-  Widget build(BuildContext context) {
+  _ConverterScreenState createState() => _ConverterScreenState();
+}
+  
+  class _ConverterScreenState extends State<ConverterScreen>{
+    @override
+    Widget build(BuildContext context) {
     final unitWidgets = units.map((Unit unit){
-      //TODO  set color to this  container
-    return Container(
-      color: Colors.black12,
-      margin: EdgeInsets.all(8.0),
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          Text(
-            unit.name,
-            style: Theme.of(context).textTheme.headline,
-          ),
-          Text(
-            'Conversion: ${unit.conversion}',
-            style: Theme.of(context).textTheme.subhead,
-
+      return Container(
+        color: Colors.black12,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subhead,
           )
         ],
       ),
